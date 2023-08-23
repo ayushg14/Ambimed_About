02328429b48219cbbe77711d8e90a1ef9838937f
AyushG <ayushg1408@gmail.com>
2023-08-23 15:39:36 +0530

    New File Added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className="text-xl font-bold text-red-500">
+        Hello Tailwind CSS with React Vite
+      </p>
+    </>
+  )
+}
+
+export default App
